4186ed6f6505a8edee314564995aad05413f6a0f
hnakamura5 <k.nakamura.hirofumi@gmail.com>
2025-08-03 23:21:22 +0900

    Modified async function

--- test/Grammar/stmt_def.py
+++ test/Grammar/stmt_def.py
@@ -94,3 +94,19 @@ def test_stmt_def_token_typed():
 
 def test_stmt_def_typed():
     assert_ast_equals(code_def_typed, result_def_typed)
+
+
+code_def_async_typed = """
+async def func(x: int) -> None {
+    return None;
+}
+"""
+result_def_async_typed = """
+async def func(x: int) -> None:
+    return None
+
+"""
+
+
+def test_stmt_def_async_typed():
+    assert_ast_equals(code_def_async_typed, result_def_async_typed)
