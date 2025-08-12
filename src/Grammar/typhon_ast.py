61470e595abb708f2b1fd444aa8b951b01d37ed5
Hirofumi Nakamura <k.nakamura.hirofumi@gmail.com>
2025-08-12 17:18:07 +0900

    Implemented function literal

--- src/Grammar/typhon_ast.py
+++ src/Grammar/typhon_ast.py
@@ -1,7 +1,7 @@
 # Ast Extensions for Typhon
 
 import ast
-from typing import Union
+from typing import Union, Unpack, TypedDict
 
 # Normal assignments, let assignments for variable declarations,
 # and constant assignments for constant definitions.
@@ -98,7 +98,7 @@ def get_type_annotation(node: ast.AST):
     return getattr(node, "type_annotation")
 
 
-def withitem_from_assignment(assign: Union[ast.Assign, ast.AnnAssign]):
+def withitem_from_assignment(assign: Union[ast.Assign, ast.AnnAssign]) -> ast.withitem:
     if isinstance(assign, ast.Assign):
         # TODO when assignment target has several target?
         return ast.withitem(context_expr=assign.value, optional_vars=assign.targets[0])
@@ -108,3 +108,50 @@ def withitem_from_assignment(assign: Union[ast.Assign, ast.AnnAssign]):
         result = ast.withitem(context_expr=assign.value, optional_vars=assign.target)
         set_type_annotation(result, assign.annotation)
         return result
+
+
+class PosAttributes(TypedDict):
+    lineno: int
+    col_offset: int
+    end_lineno: int | None
+    end_col_offset: int | None
+
+
+def get_function_literal_def(name: ast.Name) -> ast.FunctionDef:
+    return getattr(name, "func_def")
+
+
+def is_function_literal(name: ast.Name) -> bool:
+    return getattr(name, "func_def", None) is not None
+
+
+def set_function_literal_def(name: ast.Name, func_def: ast.FunctionDef):
+    setattr(name, "func_def", func_def)
+
+
+def make_function_literal(
+    args: ast.arguments,
+    returns: ast.expr,
+    body: Union[list[ast.stmt], ast.expr],
+    **kwargs: Unpack[PosAttributes],
+) -> ast.Name:
+    func_id = "temp"  # TODO: Get unique name
+    body_stmts: list[ast.stmt]
+    if isinstance(body, list):
+        body_stmts = body
+    else:
+        body_stmts = [
+            ast.Return(
+                body,
+                lineno=body.lineno,
+                col_offset=body.col_offset,
+                end_lineno=body.end_lineno,
+                end_col_offset=body.end_col_offset,
+            )
+        ]
+    func_def = ast.FunctionDef(
+        func_id, args, body_stmts, [], returns, type_comment=None, **kwargs
+    )
+    name = ast.Name(func_id, **kwargs)
+    set_function_literal_def(name, func_def)
+    return name
