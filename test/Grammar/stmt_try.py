71c92c24c62d049c26d81f99f3152e9d6c3159ce
Hirofumi Nakamura <k.nakamura.hirofumi@gmail.com>
2025-08-16 18:19:33 +0900

    Modified try statement

---
+++ test/Grammar/stmt_try.py
@@ -0,0 +1,103 @@
+from .assertion_utils import assert_ast_equals
+
+try_code = """
+try {
+    raise TypeError;
+} except {
+    raise
+}
+"""
+try_result = """
+try:
+    raise TypeError
+except:
+    raise
+"""
+
+
+def test_stmt_try():
+    assert_ast_equals(try_code, try_result)
+
+
+try_finally_code = """
+try {
+    raise TypeError;
+} except {
+    raise
+} finally {
+    cleanup();
+}
+"""
+try_finally_result = """
+try:
+    raise TypeError
+except:
+    raise
+finally:
+    cleanup()
+"""
+
+
+def test_stmt_try_finally():
+    assert_ast_equals(try_finally_code, try_finally_result)
+
+
+try_except_else_code = """
+try {
+    raise TypeError;
+} except {
+    raise
+} else {
+    cleanup();
+}
+"""
+try_except_else_result = """
+try:
+    raise TypeError
+except:
+    raise
+else:
+    cleanup()
+"""
+
+
+def test_stmt_try_except_else():
+    assert_ast_equals(try_except_else_code, try_except_else_result)
+
+
+try_except_error_code = """
+try {
+    raise TypeError;
+} except(e: TypeError) {
+    raise
+}
+"""
+try_except_error_result = """
+try:
+    raise TypeError
+except TypeError as e:
+    raise
+"""
+
+
+def test_stmt_try_except_error():
+    assert_ast_equals(try_except_error_code, try_except_error_result)
+
+
+try_except_star_code = """
+try {
+    raise TypeError;
+} except*(e: TypeError) {
+    raise
+}
+"""
+try_except_star_result = """
+try:
+    raise TypeError
+except* TypeError as e:
+    raise
+"""
+
+
+def test_stmt_try_except_star():
+    assert_ast_equals(try_except_star_code, try_except_star_result)
