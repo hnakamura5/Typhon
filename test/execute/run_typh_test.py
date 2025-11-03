d1ca89a494353aace70503a7b4a1596d8e7bf26b
hnakamura5 <k.nakamura.hirofumi@gmail.com>
2025-11-03 23:47:45 +0900

    Implemented dir test and some test case

--- test/execute/run_typh_test.py
+++ test/execute/run_typh_test.py
@@ -1,14 +1,21 @@
 from pathlib import Path
-from ...src.Typhon.Driver.run import run_file
+from ...src.Typhon.Driver.run import run_file, run_directory
 from ...src.Typhon.Driver.debugging import debug_print
 from .file_check_util import assert_file_stdout
 
 
 # Test to run .typh files in test/RunFileTest directory.
 # This is test for translation and run_file functionality.
-def test_typh_tests():
+def test_typh_files():
     test_dir = Path(__file__).parent / "RunFileTest"
     for test in test_dir.glob("*.typh"):
         result = run_file(test, capture_output=True)
         debug_print(f"Test file: {test} result:\n{result}")
         assert_file_stdout(test, result.stdout)
+
+
+def test_typh_directory():
+    test_dir = Path(__file__).parent / "RunDirTest" / "test_module"
+    result = run_directory(test_dir, capture_output=True)
+    debug_print(f"Test directory: {test_dir} result:\n{result}")
+    assert_file_stdout(test_dir / "__main__.typh", result.stdout)
