4a988118e1f5fb5cc919145cdfef61064c264a15
Hirofumi Nakamura <k.nakamura.hirofumi@gmail.com>
2026-01-03 00:05:37 +0900

    Modified try except error recovery

--- test/Grammar/Parser/invalid_syntax_error.py
+++ test/Grammar/Parser/invalid_syntax_error.py
@@ -1,18 +1,5 @@
 from ..assertion_utils import assert_parse_first_error, with_parser_verbose
 
-invalid_if_paren_code = """
-if True {print("Hello");}
-"""
-invalid_if_brace_code = """
-if (True) print("Hello");
-"""
-
-
-def test_invalid_if():
-    assert_parse_first_error(invalid_if_paren_code, SyntaxError, "expected '('")
-    assert_parse_first_error(invalid_if_brace_code, SyntaxError, "expected '{'")
-
-
 invalid_if_let_code = """
 if (let x = 10) print(x);
 """
@@ -20,18 +7,6 @@ if (let x = 10) print(x);
 
 def test_invalid_if_let():
     assert_parse_first_error(invalid_if_let_code, SyntaxError, "expected '{'")
-
-
-invalid_elif_code = """
-if (True) {
-    print("Hello");
-} elif (False) print("World");
-}
-"""
-
-
-def test_invalid_elif():
-    assert_parse_first_error(invalid_elif_code, SyntaxError, "expected '{'")
 
 
 invalid_while_paren_code = """
@@ -54,69 +29,6 @@ while (let x = 10) print(x)
 
 def test_invalid_while_let():
     assert_parse_first_error(invalid_while_let_code, SyntaxError, "expected '{'")
-
-
-invalid_for_paren_code = """
-for let i in range(10) {
-    print(i);
-}
-"""
-
-
-def test_invalid_for_paren():
-    assert_parse_first_error(invalid_for_paren_code, SyntaxError, "expected '('")
-
-
-invalid_with_brace_code = """
-with (let x = open('file.txt')) print(x); }
-"""
-
-
-def test_invalid_with_brace():
-    assert_parse_first_error(invalid_with_brace_code, SyntaxError, "expected '{'")
-
-
-invalid_try_brace_code = """
-try print("Hello");
-"""
-
-
-def test_invalid_try_brace():
-    assert_parse_first_error(invalid_try_brace_code, SyntaxError, "expected '{'")
-
-
-invalid_except_paren_code = """
-try {} except ValueError print("Error");
-"""
-invalid_except_brace_code = """
-try {
-    print("Hello");
-} except (ValueError) print("Error");
-"""
-invalid_except_star_none_code = """
-try {
-    print("Hello");
-} except * {print("Error")}
-"""
-
-
-def test_invalid_except():
-    assert_parse_first_error(invalid_except_paren_code, SyntaxError, "expected '{'")
-    assert_parse_first_error(invalid_except_brace_code, SyntaxError, "expected '{'")
-    assert_parse_first_error(
-        invalid_except_star_none_code, SyntaxError, "expected one or more"
-    )
-
-
-invalid_finally_brace_code = """
-try {
-    print("Hello");
-} finally print("Done");
-"""
-
-
-def test_invalid_finally_brace():
-    assert_parse_first_error(invalid_finally_brace_code, SyntaxError, "expected '{'")
 
 
 invalid_as_pattern_code = """
@@ -173,22 +85,3 @@ def test_invalid_function_def():
     )
     # TODO:
     # assert_parse_first_error(invalid_function_def_no_type_code, SyntaxError, "use '->'")
-
-
-invalid_class_code = """
-class MyClass
-    def __init__(self) {
-        pass;
-    }
-"""
-invalid_class_sub_code = """
-class MyClass(ParentClass)
-    def __init__(self) {
-        pass;
-    }
-"""
-
-
-def test_invalid_class_def():
-    assert_parse_first_error(invalid_class_code, SyntaxError, "expected '{'")
-    assert_parse_first_error(invalid_class_sub_code, SyntaxError, "expected '{'")
--- test/Grammar/Parser/stmt_try.py
+++ test/Grammar/Parser/stmt_try.py
@@ -1,4 +1,10 @@
-from ..assertion_utils import assert_parse, with_parser_verbose
+from ..assertion_utils import (
+    assert_parse,
+    with_parser_verbose,
+    assert_parse_error_recovery,
+    Range,
+    Pos,
+)
 
 try_code = """
 try {
@@ -101,3 +107,110 @@ except* TypeError as e:
 
 def test_stmt_try_except_star():
     assert_parse(try_except_star_code, try_except_star_result)
+
+
+try_braceless_code = """
+try print("Hello") except (e) { print("Error") }
+"""
+try_braceless_recovery = """
+try:
+    print('Hello')
+except e:
+    print('Error')
+"""
+
+_try_braceless_code = """
+try print("Hello") except { print("Error") }
+"""
+_try_braceless_recovery = """
+try:
+    print('Hello')
+except e:
+    print('Error')
+"""
+# TODO: Is this recovery desirable?
+
+
+def test_stmt_try_braceless_recovery():
+    assert_parse_error_recovery(
+        try_braceless_code,
+        try_braceless_recovery,
+        [
+            ("expected '{'", Range(Pos(1, 4), Pos(1, 5))),
+            ("expected '}'", Range(Pos(1, 18), Pos(1, 19))),
+        ],
+    )
+
+
+except_parenless_code = """
+try {} except ValueError print("Error");
+"""
+except_parenless_recover = """
+try:
+    pass
+except ValueError:
+    print('Error')
+"""
+
+
+def test_stmt_except_parenless_recovery():
+    assert_parse_error_recovery(
+        except_parenless_code,
+        except_parenless_recover,
+        [
+            ("expected '('", Range(Pos(1, 13), Pos(1, 14))),
+            ("expected ')'", Range(Pos(1, 24), Pos(1, 25))),
+            ("expected '{'", Range(Pos(1, 25), Pos(1, 26))),
+            ("expected '}'", Range(Pos(1, 39), Pos(1, 40))),
+        ],
+    )
+
+
+except_braceless_code = """
+try {
+    print("Hello");
+} except (ValueError) print("Error");
+"""
+except_braceless_recover = """
+try:
+    print('Hello')
+except ValueError:
+    print('Error')
+"""
+
+
+def test_stmt_except_braceless_recovery():
+    assert_parse_error_recovery(
+        except_braceless_code,
+        except_braceless_recover,
+        [
+            ("expected '{'", Range(Pos(3, 22), Pos(3, 23))),
+            ("expected '}'", Range(Pos(3, 36), Pos(3, 37))),
+        ],
+    )
+
+
+except_star_none_code = """
+try {
+    print("Hello");
+} except * {print("Error")}
+"""
+except_star_none_recover = """
+try:
+    print('Hello')
+except*:
+    print('Error')
+"""
+
+
+def test_stmt_except_star_none_recovery():
+    assert_parse_error_recovery(
+        except_star_none_code,
+        except_star_none_recover,
+        [
+            ("expected one or more", Range(Pos(3, 2), Pos(3, 27))),
+            ("expected '('", Range(Pos(3, 10), Pos(3, 11))),
+            ("expected ')'", Range(Pos(3, 10), Pos(3, 11))),
+            # TODO: Are they too many?
+        ],
+    )
