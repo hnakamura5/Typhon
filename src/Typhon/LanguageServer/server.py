bd602c0ede6baaa3a86146e6cd4a974e3c47188f
hnakamura5 <k.nakamura.hirofumi@gmail.com>
2026-04-29 19:04:11 +0900

    v0.2.0

--- src/Typhon/LanguageServer/server.py
+++ src/Typhon/LanguageServer/server.py
@@ -354,7 +354,7 @@ class LanguageServer(PyglsLanguageServer):
         debug_file_write(lambda: "Workspace preload completed.")
 
 
-server = LanguageServer("typhon-language-server", "v0.1.5")
+server = LanguageServer("typhon-language-server", "v0.2.0")
 client = server.backend_client
 
 
